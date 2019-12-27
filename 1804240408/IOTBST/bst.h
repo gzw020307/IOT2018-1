03ccf5fa675014ace7257e74abe329a9a8ee7d1f
VioletForever <48313605+VioletForever@users.noreply.github.com>
2019-12-27 14:36:11 +0800

    Add files via upload

---
+++ 1804240408/IOTBST/bst.h
@@ -0,0 +1,18 @@
+#ifndef BST_H
+#define BST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int key;
+    struct node *left, *right;
+};
+
+struct node *newNode(int n);
+struct node *insert(struct node *root, int n);
+void inOrder(struct node *root);
+void preOrder(struct node *root);
+struct node *delete(struct node *root, int n);
+struct node *maxValue(struct node *root);
+
+#endif
