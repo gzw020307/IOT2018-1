1988fd8e91f429abfefaefc5ad5c6402943a4dce
UmiUe <Sungrazer@foxmail.com>
2019-12-27 15:36:07 +0800

    12/27/19

--- 物联网11801-曾时铸-180240231/test1/test.c
+++ 物联网11801-曾时铸-180240231/test1/test.c
@@ -1,14 +1,14 @@
-// #include <stdio.h>
-// #include "sort.h"
+#include <stdio.h>
+#include "sort.h"
 
-// #define N 7
+#define N 7
 
-// int main()
-// {
-//     int a[N] = {-2, 4, 7, -3, 9, 1, 6};
-//     quickSort(a,N);
+int main()
+{
+    int a[N] = {-2, 4, 7, -3, 9, 1, 6};
+    quickSort(a,N);
 
-//     printArray(a,N);
+    printArray(a,N);
 
-//     return 0;
-// }
+    return 0;
+}
